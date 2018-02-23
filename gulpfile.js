b20f569f556f5376d9e4c7767219f2bf9f999542
Michael Michailidis <mmichailidis@theworkshop.com>
2018-02-23 14:38:48 +0100

    Running from app folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,68 @@
-const childProcess = require('child_process');
+const child_process = require('child_process');
 const electron = require('electron');
 const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
-const concat = require('gulp-concat');
+const css = require('gulp-css');
+const packager = require('electron-packager');
+const util = require('gulp-util');
+const pkg = require('./package.json');
+
+const destination = 'app/';
 
 const babelrc = {
     presets: ['babel-preset-es2015', 'babel-preset-react'].map(require.resolve)
 };
 
 gulp.task('copy', () => {
-    return gulp.src('src/index.html').pipe(gulp.dest('dist'));
+    return gulp.src('src/index.html').pipe(gulp.dest(destination));
+});
+
+gulp.task('build-css', function(){
+    return gulp.src('src/**/*.css')
+        .pipe(css())
+        .pipe(gulp.dest(destination));
 });
 
 gulp.task('build-main', () => {
     return gulp.src('main.js')
         .pipe(sourcemaps.init())
         .pipe(babel(babelrc))
-        .pipe(concat('main.js'))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(destination));
 });
 
 gulp.task('build-js', () => {
         return gulp.src('src/**/*.js')
             .pipe(sourcemaps.init())
             .pipe(babel(babelrc))
-            .pipe(concat('index.js'))
             .pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest('dist'));
+            .pipe(gulp.dest(destination));
 });
 
-gulp.task('build', ['build-main', 'build-js']);
+gulp.task('build', ['build-css', 'build-main', 'build-js']);
 
-gulp.task('start', ['build', 'copy'], () => {
-    childProcess.spawn(electron, ['.'], { stdio: 'inherit' })
+gulp.task('start', ['copy', 'build'], () => {
+    child_process.spawn(electron, ['.'], { stdio: 'inherit' })
         .on('close', () => process.exit());
+});
+
+let packagerOptions = {
+    platform: 'darwin',
+    arch: 'all',
+    asar: true,
+    dir: './build',
+    icon: './assets/icon.icns',
+    name: pkg.name,
+    out: './dist',
+    overwrite: true,
+    prune: true,
+    version: pkg.electronVersion,
+    'app-version': pkg.version
+};
+gulp.task('package', ['copy', 'build'], () => {
+
+    packager(packagerOptions, (err, appPath) => {
+
+    });
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.js';
+
+window.onload = () => {
+    ReactDOM.render(<App />, document.getElementById('app'));
+};
