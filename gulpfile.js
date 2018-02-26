1b9d6e501e4047bb086544f9bb44b231211d873a
Michael Michailidis <mmichailidis@theworkshop.com>
2018-02-26 13:58:17 +0100

    Added first test

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,9 @@ const maps  = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const css   = require('gulp-css');
 
+/* Build */
+
 const destination = 'app/';
-
-// const babelrc = {
-//     presets: ['babel-preset-es2015', 'babel-preset-react'].map(require.resolve)
-// };
-
-gulp.task('copy', () => {
-    return gulp.src('src/index.html').pipe(gulp.dest(destination));
-});
 
 gulp.task('build-css', function(){
     return gulp.src('src/**/*.css')
@@ -20,36 +14,45 @@ gulp.task('build-css', function(){
         .pipe(gulp.dest(destination));
 });
 
-gulp.task('build-main', () => {
-    return gulp.src('main.js')
-        .pipe(maps.init())
-        .pipe(babel())
-        .pipe(maps.write('.'))
-        .pipe(gulp.dest(destination));
-});
-
 gulp.task('build-js', () => {
-        return gulp.src('src/**/*.js')
+        return gulp.src(['main.js', 'src/**/*.js', '!src/**/*.test.js'])
             .pipe(maps.init())
             .pipe(babel())
             .pipe(maps.write('.'))
             .pipe(gulp.dest(destination));
 });
 
-gulp.task('build', ['build-css', 'build-main', 'build-js']);
 
-const exit = () => process.exit();
+gulp.task('build', ['build-css', 'build-js']);
+
+
+/* Copy */
+
+gulp.task('copy-html', () => {
+    return gulp.src('src/index.html').pipe(gulp.dest(destination));
+});
+
+gulp.task('copy', ['copy-html']);
+
+
+
+/* Execute */
+
+const cmd   = (name) => 'node_modules/.bin/' + name;
+const args  = (more) => Array.isArray(more) ? ['.'].concat(more) : ['.'];
+const exit  = () => process.exit();
+
 gulp.task('start', ['copy', 'build'], () => {
-    spawn('node_modules/.bin/electron', ['.'], { stdio: 'inherit' }).on('close', exit);
+    spawn(cmd('electron'), args(), { stdio: 'inherit' }).on('close', exit);
 });
 
 
 gulp.task('release', ['copy', 'build'], () => {
-    spawn('node_modules/.bin/electron-builder', ['.'], { stdio: 'inherit' }).on('close', exit);
+    spawn(cmd('electron-builder'), args(), { stdio: 'inherit' }).on('close', exit);
 });
 
 gulp.task('test', ['copy', 'build'], () => {
-    spawn('node_modules/.bin/jest', ['.'], { stdio: 'inherit' }).on('close', exit);
+    spawn(cmd('jest'), args(), { stdio: 'inherit' }).on('close', exit);
 });
 
 
