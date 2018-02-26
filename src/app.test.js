1249f4f7f3581ff211a2ee88084bffbd369b1fc6
Michael Michailidis <mmichailidis@theworkshop.com>
2018-02-26 14:14:19 +0100

    Added tests

--- src/app.test.js
+++ src/app.test.js
@@ -1,22 +1,10 @@
+import React from 'react';
 import App from './app';
 import renderer from 'react-test-renderer';
 
-test('Link changes the class when hovered', () => {
-    const component = renderer.create(
-        <Link page="http://www.facebook.com">Facebook</Link>,
-    );
+test('App is working', () => {
+    const component = renderer.create(<App />);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 
-    // manually trigger the callback
-    tree.props.onMouseEnter();
-    // re-rendering
-    tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-
-    // manually trigger the callback
-    tree.props.onMouseLeave();
-    // re-rendering
-    tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
 });
