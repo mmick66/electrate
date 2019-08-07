4cf89a7d7fa866577a135c2df356480ebd7c930e
Michael Michailidis <mmick66@gmail.com>
2019-08-07 21:12:21 +0300

    Simplified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,38 @@
 const spawn = require('child_process').spawn;
-const gulp  = require('gulp');
-const maps  = require('gulp-sourcemaps');
+const gulp = require('gulp');
 const babel = require('gulp-babel');
-const css   = require('gulp-css');
-const path   = require('path');
-
-/* Build */
-
-gulp.task('build-css', function(){
+const css = require('gulp-css');
+// 1. Copy the index.html as is
+gulp.task('html', () => {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('app/'));
+});
+// 2. Compile CSS file and move them to the app folder
+gulp.task('css', () => { // 2.
     return gulp.src('src/**/*.css')
         .pipe(css())
         .pipe(gulp.dest('app/'));
 });
 
-gulp.task('build-js', () => {
-        return gulp.src(['main.js', 'src/**/*.js', '!src/**/*.test.js'])
-            .pipe(maps.init())
-            .pipe(babel())
-            .pipe(maps.write('.'))
-            .pipe(gulp.dest('app/'));
+// 3. Compile JS files and move them to the app folder
+gulp.task('js', () => { // 3.
+    return gulp.src(['main.js', 'src/**/*.js'])
+         .pipe(babel())
+         .pipe(gulp.dest('app/'));
 });
-
-
-gulp.task('build', gulp.series('build-css', 'build-js'));
-
-
-/* Copy */
-
-gulp.task('copy-html', () => {
-    return gulp.src('src/*.html').pipe(gulp.dest('app/'));
-});
-
-gulp.task('copy-assets', () => {
-    return gulp.src('assets/**/*').pipe(gulp.dest('app/assets'));
-});
-
-gulp.task('copy', gulp.parallel('copy-html', 'copy-assets'));
-
-
-
-/* Execute */
-
-const cmd   = (name) => path.join('.', 'node_modules', '.bin', name);
-const args  = (more) => Array.isArray(more) ? ['.'].concat(more) : ['.'];
-const exit  = () => process.exit();
-
-gulp.task('start', gulp.series('copy', 'build', async () => {
-    spawn(cmd('electron'), args(), { stdio: 'inherit', cwd: '.', shell: true }).on('close', exit);
+// 4. Start the electron process.
+gulp.task('start', gulp.series('html', 'css', 'js', () => { // 4.
+    spawn(
+        'node_modules/.bin/electron',
+        ['.'],
+        { stdio: 'inherit' }
+    ).on('close', () => process.exit());
 }));
 
-
-gulp.task('release', gulp.series('copy', 'build', () => {
-    spawn(cmd('electron-builder'), args(), { stdio: 'inherit', cwd: '.', shell: true }).on('close', exit);
-}));
-
-gulp.task('test', gulp.series('copy', 'build', () => {
-    spawn(cmd('jest'), args(), { stdio: 'inherit', cwd: '.', shell: true }).on('close', exit);
-}));
-
-
+// gulp.task('release', gulp.series('html', 'css', 'js', () => { // 5.
+//     spawn(
+//         'node_modules/.bin/electron-builder',
+//         ['.'],
+//         { stdio: 'inherit' }
+//     ).on('close', () => process.exit());
+// }));
