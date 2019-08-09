b9d9a1cb967aaf7398d4b978bdb0b2955d8b5711
Michael Michailidis <mmick66@gmail.com>
2019-08-09 15:19:10 +0300

    Abstracted the build function

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,27 @@ const spawn = require('child_process').spawn;
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const css = require('gulp-clean-css');
-// 1. Copy the index.html as is
+
 gulp.task('html', () => {
     return gulp.src('src/index.html')
         .pipe(gulp.dest('app/'));
 });
-// 2. Compile CSS file and move them to the app folder
-gulp.task('css', () => { // 2.
+
+gulp.task('css', () => {
     return gulp.src('src/**/*.css')
         .pipe(css())
         .pipe(gulp.dest('app/'));
 });
 
-// 3. Compile JS files and move them to the app folder
-gulp.task('js', () => { // 3.
+gulp.task('js', () => {
     return gulp.src(['main.js', 'src/**/*.js'])
          .pipe(babel())
          .pipe(gulp.dest('app/'));
 });
-// 4. Start the electron process.
-gulp.task('start', gulp.series('html', 'css', 'js', () => { // 4.
+
+gulp.task('build', gulp.series('html', 'css', 'js'));
+
+gulp.task('start', gulp.series('build', () => {
     spawn(
         'node_modules/.bin/electron',
         ['.'],
@@ -29,7 +30,7 @@ gulp.task('start', gulp.series('html', 'css', 'js', () => { // 4.
     ).on('close', () => process.exit());
 }));
 
-gulp.task('release', gulp.series('html', 'css', 'js', () => { // 5.
+gulp.task('release', gulp.series('build', () => {
     spawn(
         'node_modules/.bin/electron-builder',
         ['.'],
