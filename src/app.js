e8032fe8a979a2bef91f90b3a8445d0680fd297c
Kevin Hashimoto <khash615@gmail.com>
2020-01-09 02:12:16 -0800

    fixes file name typo that prevents it from appearing in production

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
         return (
             <div className={'hello'}>
                 <h2>Hello Electrate</h2>
-                <img src="assets/Logo.png" />
+                <img src="assets/logo.png" />
                 <h4>A basic Electron + React.js template</h4>
                 <h4>Have Fun!</h4>
             </div>
